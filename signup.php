3c89c5445b683dfe8bf46b099250c2ffb3bf2eba
bobyswaroop <bobyswaroop555024@gmail.com>
2023-03-23 17:17:46 +0530

    some changes

--- signup.php
+++ signup.php
@@ -1,9 +1,9 @@
 
 <?php
 session_start();
-if(isset($_SESSION['email'])){
-    header('location:show.php');
-  }
+// if(isset($_SESSION['email'])){
+//     header('location:show.php');
+//   }
 ?>
 
 
